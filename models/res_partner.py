53506023082b1fdb994e85376d8f83eba3b09ee3
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-03-20 15:47:25 +0100

    Partner:Add the «Municipality» in the formatting of address

--- models/res_partner.py
+++ models/res_partner.py
@@ -4,7 +4,7 @@
 from odoo import models,fields,api
 from odoo.exceptions import UserError
 
-
+ADDRESS_FIELDS = ('street', 'street2', 'zip', 'municipality_id', 'state_id', 'country_id')
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
@@ -16,3 +16,17 @@ class ResPartner(models.Model):
         if self.municipality_id:
             self.state_id = self.municipality_id.state_id and self.municipality_id.state_id.id or False
             self.zip = self.municipality_id.zip
+
+
+    @api.model
+    def _address_fields(self):
+        """Returns the list of address fields that are synced from the parent."""
+        return list(ADDRESS_FIELDS)
+
+    def _prepare_display_address(self, without_company=False):
+        address_format, args = super(ResPartner,self)._prepare_display_address(without_company=without_company)
+        address_format = address_format.replace("(city)s","(municipality_name)s %(state_name)s")
+        municipality = args.get('municipality_id',False)
+        if municipality:
+            args.update({'municipality_name':municipality.name})
+        return address_format,args
